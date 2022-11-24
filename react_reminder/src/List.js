c32a8bd7165b527a561ee37525e03fb8ec88c57d
Ifeoluwa <ifel1939@gmail.com>
2022-11-24 17:05:47 +0100

    task completed

---
+++ react_reminder/src/List.js
@@ -0,0 +1,20 @@
+  import React from "react";
+
+export default function List({ people }) {
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h3>{name}</h3>
+              <p>{age} years</p>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
+}
